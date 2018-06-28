80d7cb9766acc9bbe584e3238f24f6d1008e18be
ingmapping <31092728+ingmapping@users.noreply.github.com>
2018-06-28 16:49:33 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# Copying viewer to data folder 
-cp /root/src/openstreets-nl/index.html /data/
 
 export MAPNIK_MAP_FILE=~/src/openstreets-nl/openstreets-nl.xml
 export MAPNIK_TILE_DIR=/data/tiles
 mkdir -p ${MAPNIK_TILE_DIR}
 chmod -R 777 ${MAPNIK_TILE_DIR}
 
+# Copying viewer to data folder 
+cp /root/src/openstreets-nl/index.html /data/tiles
+
+# Generate openstreets-nl tiles
 python /root/src/openstreets-nl/generate_tiles.py
 
 
